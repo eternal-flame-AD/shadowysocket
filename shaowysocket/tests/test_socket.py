9365cc529dd55e3829f3a71201b296caa5ae86d7
_eternal_flame <eternal_flame-AD@protonmail.com>
2018-04-29 12:29:21 +0800

    fix pep8+pytest

--- shaowysocket/tests/test_socket.py
+++ shaowysocket/tests/test_socket.py
@@ -1,43 +1,50 @@
 import os
 import sys
+import threading
+import socket
+import time
 
 
 parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-print(parentdir)
 sys.path.insert(0, parentdir)
-
-import threading
-import socket
-import asyncio
 
 import shadowysocket
 
+
 class echoserver():
-    
+
     def __init__(self):
         self.conn = socket.socket()
-        self.conn.bind(("127.0.0.1",12300))
+        self.conn.bind(("127.0.0.1", 12300))
         self.conn.listen()
         while True:
             conn, address = self.conn.accept()
             conn = shadowysocket.shadowysocket(conn)
-            threading.Thread(target=self.echo,args=(conn, address)).start()
-    
+            threading.Thread(target=self.echo, args=(conn, address)).start()
+
     def echo(self, conn, address):
-        data = conn.recv(4096)
-        conn.sendall(data + address[0].encode('utf-8'))
+        while True:
+            data = conn.recv(4096)
+            if len(data) == 0:
+                break  # connection close
+            conn.sendall(data + address[0].encode('utf-8'))
+
 
 class echoclient():
 
     def __init__(self):
         self.conn = socket.create_connection(("127.0.0.1", 12300))
         self.conn = shadowysocket.shadowysocket(self.conn)
-    
+
     def send(self, data):
         self.conn.sendall(data)
-    
+
     def recv(self):
         return self.conn.recv(4096)
+
+    def close(self):
+        self.conn.close()
+
 
 def test_socket():
     server = threading.Thread(target=echoserver)
@@ -46,4 +53,12 @@ def test_socket():
     client = echoclient()
     client.send(b"hello world")
     assert client.recv() == b"hello world127.0.0.1"
-    
+    client.send(b"bye")
+    assert client.recv() == b"bye127.0.0.1"
+    client.close()
+    time.sleep(0.5)  # wait for connection thread to die
+    assert threading.active_count() == 2  # main+server
+
+
+if __name__ == "__main__":
+    test_socket()
